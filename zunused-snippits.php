3e61c33119c9e699a21dcda7967ff69393355879
jtharpe <jtharpe@overflownetworks.com>
2020-03-27 16:57:02 -0400

    This file is things not used in the main script

---
+++ zunused-snippits.php
@@ -0,0 +1,9 @@
+//echo 'Hello World!';
+        //$query = "SELECT * FROM wp_postmeta WHERE meta_key = 'external_image_url';";
+        //echo $query;
+        //$results = $wpdb->get_row($query);
+        //echo $results->post_id;
+
+        //$test = $this->ImageFeeder();
+        //echo $test;
+            //echo "<script src='" . plugins_url('/assets/js/main.js', __FILE__) . "'></script>";
