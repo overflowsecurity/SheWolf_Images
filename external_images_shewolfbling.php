4abb9badf4dbdee505efa70b783c6536ad5f13d8
jtharpe <jtharpe@overflownetworks.com>
2020-03-28 12:09:40 -0400

    test

--- external_images_shewolfbling.php
+++ external_images_shewolfbling.php
@@ -143,7 +143,7 @@ class SheWolf_Bling_Images_Plugin{
         global $wpdb;
         foreach($ids as $id){
             $she_post_id = $id->post_id;
-            $query = 'SELECT * FROM shewolfb_wp.wp_posts WHERE post_type = "attachment" AND post_parent = ' . $she_post_id . ";";
+            $query = 'SELECT ID FROM shewolfb_wp.wp_posts WHERE post_type = "attachment" AND post_parent = ' . $she_post_id . ";";
             $test = array($wpdb->get_results($query));
             ?><br></br><?php
             echo "Post ID: " . $she_post_id;
