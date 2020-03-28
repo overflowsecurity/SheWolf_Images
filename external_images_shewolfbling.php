fec04b838ed4bdd5db4a3c83ff15a290d5032f14
jtharpe <jtharpe@overflownetworks.com>
2020-03-27 21:33:32 -0400

    cleanup

--- external_images_shewolfbling.php
+++ external_images_shewolfbling.php
@@ -123,35 +123,6 @@ class SheWolf_Bling_Images_Plugin{
 
 }
 
-class page_writer {
-
-    public static function replace_text($text) {
-    $text = str_replace('Avenue', 'boobs', $text);
-    return $text;
-}
-
-    public static function replace_images($image, $attachment_id, $size  = 'woocommerce_thumbnail', $icon) {
-        global $wpdb;
-        global $post;
-        if (is_admin()) {
-            //don't override the gallery on the admin pages, otherwise we end up saving the images to the database
-            return $image;
-        } elseif( is_object( $post ) && 10508 == $post->ID ){
-            $query = "SELECT * FROM wp_postmeta WHERE post_id = $post->ID AND meta_key = \"external_image_url\";";
-            $results = $wpdb->get_row($query);
-
-            $image[0] = $results->meta_value;
-
-            return $image;
-        } else {
-            return $image;
-        }
-    }
-
-}
-
-//add_filter('woocommerce_cart_item_name', array( 'page_writer', 'replace_text' ) );
-//add_filter('wp_get_attachment_image_src', array( 'page_writer', 'replace_images'), 10,4 );
 
 
 
