f115522daad8ffff00d257166ba22742bce53ac0
jtharpe <jtharpe@overflownetworks.com>
2020-03-27 22:11:05 -0400

    added foreach loop

--- external_images_shewolfbling.php
+++ external_images_shewolfbling.php
@@ -26,11 +26,16 @@ class SheWolf_Bling_Images_Plugin{
     global $wpdb;
     //Get image info
     $query = 'SELECT * FROM wp_postmeta WHERE meta_key = "external_image_url"';
-    $response = $wpdb->get_results($query);
+    foreach( $wpdb->get_results($query) as $key => $urls) {
+        $test = $urls->post_id;
+        echo ?></ BR> <?php
+        echo $test
+        echo ?></ BR> <?php
+    }
     //echo $response->meta_value;
-    $urls = $response->meta_value;
+    //$urls = $response->meta_value;
     //$cleanup = explode("|", $urls);
-    echo $urls;
+    //echo $urls;
     //echo $cleanup[0];
     //echo $cleanup[1];
     
