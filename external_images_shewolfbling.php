dcc3a2352febc1d8a8d3a347c12425d4207974b3
jtharpe <jtharpe@overflownetworks.com>
2020-03-28 13:11:14 -0400

    test

--- external_images_shewolfbling.php
+++ external_images_shewolfbling.php
@@ -148,21 +148,13 @@ class SheWolf_Bling_Images_Plugin{
             ?><br></br><?php
             echo "Post ID: " . $she_post_id;
             ?><br></br><?php
-
-            foreach($test as $image_id){
+            var_dump($test);
+            /* foreach($test as $image_id){
                 $test = $image_id->ID;
                 $test_array = array_fill($test);
                 ?><br></br><?php
             }
-            var_dump($test_array);
-/*             foreach( $wpdb->get_results($query) as $key => $she_image_results) {
-                $she_image_ids = $she_image_results->ID;
-                echo "Post ID: " . $she_post_id;
-                ?><br></br><?php
-                echo $she_image_ids;
-                ?><br></br><?php
-
-        } */
+            var_dump($test_array); */
 
     }
 
